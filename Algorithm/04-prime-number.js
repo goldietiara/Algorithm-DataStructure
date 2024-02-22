0752c6ea34c41de0bd6dd542b953c0d3f946b571
owy <126146587+goldietiara@users.noreply.github.com>
2024-02-22 10:48:52 +0700

    prime number

---
+++ Algorithm/04-prime-number.js
@@ -0,0 +1,35 @@
+// PROBLEM =^._.^= ∫
+// Give a natural number 'n', determine if the number is prime
+// A prime number is a numbers that have only 2 factors: 1 and themselves
+// isPrime(5) = true (1*5 or 5*1)
+// isPrime(4) = false (1*2 or 2*2 or 4*1)
+
+// OWY WORK ᕦʕ •`ᴥ•´ʔᕤ
+const isPrime = (n) => {
+  if (n < 2) {
+    return `number ${n} is false`;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return `number ${n} is false`;
+    }
+  }
+  return `number ${n} is true`;
+};
+
+console.log(isPrime(10));
+
+// THE ANSWER
+const theAnswer = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(theAnswer(11));
